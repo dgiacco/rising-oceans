06c29cfd6b074e81904943f53f3c1026e337aff9
Damian Giaccobasso <damiangiaccobasso@192.168.1.2>
2024-08-23 23:30:42 -0300

    add underwater background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import Bubbles from "@/components/Bubbles";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="underwater" style={{ overflow: "hidden" }}>
+        <Bubbles />
         <Providers>{children}</Providers>
       </body>
     </html>

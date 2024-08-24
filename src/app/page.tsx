8039214b378ae60b7ca5074664eca6df00fc9a45
Damian Giaccobasso <damiangiaccobasso@192.168.1.2>
2024-08-23 21:55:00 -0300

    initial commit and rainbowkit setup

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+export default function Home() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "flex-end",
+        padding: 12,
+      }}
+    >
+      <ConnectButton />
+    </div>
+  );
+}

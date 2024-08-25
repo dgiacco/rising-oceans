d822107ca2f4f65f17d1058b09f0fd5c0cc13059
Damian Giaccobasso <damiangiaccobasso@192.168.1.2>
2024-08-24 22:46:18 -0300

    custom rainbowkit styling

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,7 +3,13 @@
 import "@rainbow-me/rainbowkit/styles.css";
 import { ReactNode } from "react";
 
-import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+  darkTheme,
+  Theme,
+} from "@rainbow-me/rainbowkit";
+import merge from "lodash.merge";
 import { WagmiProvider } from "wagmi";
 import {
   mainnet,
@@ -22,12 +28,28 @@ const config = getDefaultConfig({
 });
 
 const queryClient = new QueryClient();
+const myCustomTheme = merge(darkTheme(), {
+  colors: {
+    modalBackground: "#005e79",
+    accentColor: "#6adbd8",
+    modalBorder: "#6adbd8",
+    closeButton: "white",
+    closeButtonBackground: "#34918e",
+    accentColorForeground: "#005e79",
+  },
+  radii: {
+    actionButton: "10px",
+    connectButton: "12px",
+  },
+});
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>{children}</RainbowKitProvider>
+        <RainbowKitProvider theme={myCustomTheme}>
+          {children}
+        </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );

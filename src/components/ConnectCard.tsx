7c026523cc1adee19ccadcb147edc6de79de013e
Damian Giaccobasso <damiangiaccobasso@192.168.1.2>
2024-08-26 01:07:57 -0300

    create animated wave loader

--- src/components/ConnectCard.tsx
+++ src/components/ConnectCard.tsx
@@ -1,27 +1,48 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import React from "react";
+import WaveLoader from "./WaveLoader";
 
 const ConnectCard = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="flex items-center justify-center">
-      <div className="border-2 border-roAquaBlue rounded-lg shadow-md p-6 max-w-lg w-full mx-4 sm:mx-auto backdrop-blur">
-        <h2 className="text-2xl sm:text-3xl font-bold text-center mb-4 text-slate-400">
-          Welcome to Rising Oceans!
-        </h2>
-        <p className="text-center text-slate-400 sm:text-lg">
-          Please connect your wallet to continue.
-        </p>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            paddingTop: 60,
-          }}
-        >
-          <ConnectButton />
+    <>
+      {isLoading ? (
+        <WaveLoader />
+      ) : (
+        <div className="flex items-center justify-center">
+          <div className="border-2 border-roAquaBlue rounded-lg shadow-md p-6 max-w-lg w-full mx-4 sm:mx-auto backdrop-blur">
+            <h2 className="text-2xl sm:text-3xl font-bold text-center mb-4 text-slate-400">
+              Welcome to Rising Oceans!
+            </h2>
+            <p className="text-center text-slate-400 sm:text-lg">
+              Please connect your wallet to continue.
+            </p>
+
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                paddingTop: 60,
+              }}
+            >
+              <ConnectButton />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 

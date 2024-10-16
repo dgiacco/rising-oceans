5feab359af1be0e5538a63e76c84b6f1881bf0c4
Damian Giaccobasso <damiangiaccobasso@192.168.1.2>
2024-10-16 14:47:59 -0300

    improve home styling for mobile and min input height

--- frontend/src/app/(general)/home/page.tsx
+++ frontend/src/app/(general)/home/page.tsx
@@ -13,11 +13,24 @@ const HomePage = () => {
 
   return (
     <div className="my-5 px-4 sm:px-6 lg:px-8 mx-auto max-w-7xl">
-      <div className="flex justify-between items-center mb-3">
-        <h2 className="relative text-center text-3xl sm:text-4xl lg:text-5xl font-bold text-roSeaGreen brightness-125">
-          Active campaigns
+      <div className="flex flex-col sm:flex-row justify-between items-center mb-3">
+        <div className="sm:hidden mb-3">
+          <Button
+            label="Start a campaign"
+            variant="primary"
+            onClick={goToForm}
+          />
+        </div>
+        <h2 className="relative text-center text-3xl sm:text-4xl lg:text-5xl font-bold text-roSeaGreen brightness-125 mb-3 sm:mb-0 order-2 sm:order-1">
+          <span className="text-4xl lg:text-5xl">Active campaigns</span>
         </h2>
-        <Button label="Start a campaign" variant="primary" onClick={goToForm} />
+        <div className="hidden sm:block order-1 sm:order-2">
+          <Button
+            label="Start a campaign"
+            variant="primary"
+            onClick={goToForm}
+          />
+        </div>
       </div>
 
       <OrgCardList />
--- frontend/src/components/CampaignForm.tsx
+++ frontend/src/components/CampaignForm.tsx
@@ -143,9 +143,9 @@ const CampaignForm = () => {
 
   const inputLabel = "text-roSeaGreen mt-2 text-center";
   const formInput =
-    "mt-1 p-2 bg-roAquaBlue/20 border border-2 border-roSeaGreen rounded-lg w-full text-roAquaBlue font-bold min-h-[40px]";
+    "mt-1 p-2 bg-roAquaBlue/20 border border-2 border-roSeaGreen rounded-lg w-full text-roAquaBlue font-bold min-h-[46px]";
   const targetInput =
-    "mt-1 p-2 bg-roAquaBlue/20 border border-2 rounded-lg w-full font-bold min-h-[40px]";
+    "mt-1 p-2 bg-roAquaBlue/20 border border-2 rounded-lg w-full font-bold min-h-[46px]";
 
   const handleCloseModal = () => {
     setIsTxConfirmed(false);
